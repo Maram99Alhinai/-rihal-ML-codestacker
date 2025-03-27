54d02ade19b78a1c099eef62673f953427df3bce
Maram99Alhinai <physics444maram@gmail.com>
2025-03-28 00:04:26 +0400

    save the plots as htmls

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -1,25 +1,10 @@
 from flask import Flask, render_template
-import pandas as pd
-import plotly.express as px
-import folium
+
 
 
 app = Flask(__name__)
 
-df = pd.read_csv('Competition_Dataset.csv')
-df['Dates'] = pd.to_datetime(df['Dates'])
-df['hour'] = df['Dates'].dt.hour
 
-
-# Count crimes per hour
-crime_by_hour = df.groupby('hour').size().reset_index(name='crime_count')
-# Create histogram with adjustable bins
-fig_1 = px.histogram(df, x='hour', nbins=12,  
-                   title='Crime Traffic During the Day',
-                   labels={'hour': 'Hour of the Day', 'count': 'Number of Crimes'},
-                   color_discrete_sequence=['skyblue'])
-fig_1.update_traces(marker=dict(line=dict(color='black', width=1.5)))
-plot_html = fig_1.to_html(full_html=False)
 
 
 
